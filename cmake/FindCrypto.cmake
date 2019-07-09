da5fc38f9ee1cca065a3fd841fc3c126f5f69e56
Boris.Dergachov <admin@froexilize.com>
2019-07-09 12:32:20 +0300

    Updates install rules

---
+++ cmake/FindCrypto.cmake
@@ -0,0 +1,37 @@
+find_path(
+		CRYPTO_INCLUDE_DIR
+		NAMES crypto.h
+		PATHS /usr/local/include /usr/include ${CRYPTO_DIR}/include
+)
+
+message(STATUS "CRYPTO_DIR=${CRYPTO_DIR}")
+
+if(CRYPTO_INCLUDE_DIR)
+	find_library(CRYPTO_LIBRARY
+			NAMES ra_lib
+			PATHS
+				/usr/local/lib
+				/usr/lib
+				${CRYPTO_DIR}/lib
+	)
+	if(CRYPTO_LIBRARY)
+		set(CRYPTO_LIBRARY_DIR "")
+		get_filename_component(CRYPTO_LIBRARY_DIRS ${CRYPTO_LIBRARY} PATH)
+		set(CRYPTO_FOUND ON)
+		set(CRYPTO_INCLUDE_DIRS ${CRYPTO_INCLUDE_DIR})
+		set(CRYPTO_LIBRARIES ${CRYPTO_LIBRARY})
+	endif()
+else()
+	message(STATUS "FindCrypto: Could not find crypto.h")
+endif()
+
+if(CRYPTO_FOUND)
+	if(NOT CRYPTO_FIND_QUIETLY)
+		message(STATUS "FindCrypto: Found crypto.h and ra_lib")
+	endif()
+else()
+	if(CRYPTO_FIND_REQUIRED)
+		message(FATAL_ERROR "FindCrypto: Could not find crypto.h and/or ra_lib")
+	endif()
+endif()
+
